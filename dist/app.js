5e7a1da004460e1a0c3c06ddb2212939612bc480
tobymould <toby.mould@icloud.com>
2020-03-19 15:01:47 +0000

    fixed Javascript bug on selected seats

--- dist/app.js
+++ dist/app.js
@@ -1,37 +1,64 @@
-// DOM Elements:
-const container = document.getElementById(".container");
+// 1. DOM Elements:
+const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-//Values
-const ticketPrice = +movieSelect.value; //'+' turns string datatype into integer.
+//2. Values
+let ticketPrice = +movieSelect.value; //'+' turns string datatype into integer.
 
-// Functions:
+// 3. Support Functions:
 // updates total and count
 function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll(".row .seats .selected");
+  const selectedSeats = document.querySelectorAll(".row .seats.selected");
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-// Event Listener:
+// 4. Event Listener:
+// Movie Select Event:
+movieSelect.addEventListener("change", e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+// Seat Click Event:
 container.addEventListener("click", e => {
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
   ) {
-    //accesses the target classlist if it contains 'seat' and NOT contain 'occupied'.
     e.target.classList.toggle("selected");
+
+    updateSelectedCount();
   }
+});
 
-  updateSelectedCount();
-}); //...toggle the classlist of the target element to include 'selected' temporarily while selected.
+// Initial count and total set
+updateSelectedCount();
+//accesses the target classlist if it contains 'seat' and NOT contain 'occupied'.
+//...toggle the classlist of the target element to include 'selected' temporarily while selected.
 
+//
+//
 /* Lessons Learned
+> How to structure a Javascript file:
+    1. GRAB DOM Elements  - Grab all the elements you wanna apply JS to:
+        a. -
+    2. GRAB DOM Values    - If you assigned any 'value' properties to elements, can grab here if need them.
+    3. Support Functions  - Create all of the functions you will run inside the Event Listeners Handler (function): 
+        a. -
+        b. -
+        c. -
+    4. EventListeners     - Apply to the container (parent) of the area of interest (child/children): 
+        a. Set up 'if-conditional' criteria, looking at the 'classList' of the 'event parameter-target' 
+        b. Afterward, CALLOUT the support functions in order of use to do specific tasks.
+
+
 > You should apply the event listener to the parent container element instead of any of the children. 
+
+> '+' - put as prefix on variable name to ensure turns datatype 'string' into 'integer'.
 
 > classList.
     a) .add    - Adds permanently to the classList of that element once activated.
